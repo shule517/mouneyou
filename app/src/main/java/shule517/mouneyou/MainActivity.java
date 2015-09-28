192e0b8a5c5f799c6004e0e17370544604bbd09c
shule517 <shule517@gmail.com>
2015-09-28 22:36:50 +0900

    Twitter4jを導入

--- app/src/main/java/shule517/mouneyou/MainActivity.java
+++ app/src/main/java/shule517/mouneyou/MainActivity.java
@@ -30,171 +30,171 @@ public class MainActivity extends AppCompatActivity {
         list = new ArrayList<ListItem>();
 
         String[][] stringList =
-                {
-                        {
-                                "/images/stamp/stamp001.png",
-                                "http://pic.twitter.com/zJf9UxVkCy"
-                        }, {
-                        "/images/stamp/stamp002.png",
-                        "http://pic.twitter.com/gGWnG2T1N2"
-                }, {
-                        "/images/stamp/stamp003.png",
-                        "http://pic.twitter.com/8dnYyZqEoF"
-                }, {
-                        "/images/stamp/stamp004.png",
-                        "http://pic.twitter.com/zjx4PUhbmY"
-                }, {
-                        "/images/stamp/stamp005.png",
-                        "http://pic.twitter.com/kHnGxG95CI"
-                }, {
-                        "/images/stamp/stamp006.png",
-                        "http://pic.twitter.com/SbRtVCDOit"
-                }, {
-                        "/images/stamp/stamp007.png",
-                        "http://pic.twitter.com/zIdqC5MtxE"
-                }, {
-                        "/images/stamp/stamp008.png",
-                        "http://pic.twitter.com/yYZEcqQCGZ"
-                }, {
-                        "/images/stamp/stamp009.png",
-                        "http://pic.twitter.com/dYwZ3axpKW"
-                }, {
-                        "/images/stamp/stamp010.png",
-                        "http://pic.twitter.com/Z0yAX0QcHJ",
-                }, {
-                        "/images/stamp/stamp011.png",
-                        "http://pic.twitter.com/M6I6regjKd",
-                }, {
-                        "/images/stamp/stamp012.png",
-                        "http://pic.twitter.com/Iv8gvPUATM"
-                }, {
-                        "/images/stamp/stamp013.png",
-                        "http://pic.twitter.com/sQOagjwDRJ"
-                }, {
-                        "/images/stamp/stamp014.png",
-                        "http://pic.twitter.com/1iWz6NfP9t"
-                }, {
-                        "/images/stamp/stamp015.png",
-                        "http://pic.twitter.com/tyXVCqpQTc"
-                }, {
-                        "/images/stamp/stamp016.png",
-                        "http://pic.twitter.com/e1CILuyhgc"
-                }, {
-                        "/images/stamp/stamp017.png",
-                        "http://pic.twitter.com/jmUVSC3zXK"
-                }, {
-                        "/images/stamp/stamp018.png",
-                        "http://pic.twitter.com/2pd753IzXC"
-                }, {
-                        "/images/stamp/stamp019.png",
-                        "http://pic.twitter.com/e1B9k2MXgS"
-                }, {
-                        "/images/stamp/stamp020.png",
-                        "http://pic.twitter.com/wGwLC41tdH"
-                }, {
-                        "/images/stamp/stamp021.png",
-                        "http://pic.twitter.com/SYkLTqipqL"
-                }, {
-                        "/images/stamp/stamp022.png",
-                        "http://pic.twitter.com/wb79W80JeH"
-                }, {
-                        "/images/stamp/stamp023.png",
-                        "http://pic.twitter.com/YYwHReTUBS",
-                }, {
-                        "/images/stamp/stamp024.png",
-                        "http://pic.twitter.com/VnmJEufqY2"
-                }, {
-                        "/images/stamp/stamp025.png",
-                        "http://pic.twitter.com/6viZHnOBEn"
-                }, {
-                        "/images/stamp/stamp026.png",
-                        "http://pic.twitter.com/MMDWi2jWHB"
-                }, {
-                        "/images/stamp/stamp027.png",
-                        "http://pic.twitter.com/kDzFQfo6ty"
-                }, {
-                        "/images/stamp/stamp028.png",
-                        "http://pic.twitter.com/cuWqlvgib3"
-                }, {
-                        "/images/stamp/stamp029.png",
-                        "http://pic.twitter.com/RQdFyGXkcv"
-                }, {
-                        "/images/stamp/stamp030.png",
-                        "http://pic.twitter.com/4xZ45MTCNv"
-                }, {
-                        "/images/stamp/stamp031.png",
-                        "http://pic.twitter.com/M1zjqfk8pH"
-                }, {
-                        "/images/stamp/stamp032.png",
-                        "http://pic.twitter.com/agDkVgLd3n"
-                }, {
-                        "/images/stamp/stamp033.png",
-                        "http://pic.twitter.com/3wfkhxsCH1",
-                }, {
-                        "/images/stamp/stamp034.png",
-                        "http://pic.twitter.com/29RqnztSk9"
-                }, {
-                        "/images/stamp/stamp035.png",
-                        "http://pic.twitter.com/XiMVmrDUWp"
-                }, {
-                        "/images/stamp/stamp036.png",
-                        "http://pic.twitter.com/hXcB5g6Ygq"
-                }, {
-                        "/images/stamp/stamp037.png",
-                        "http://pic.twitter.com/XWopb3cOts"
-                }, {
-                        "/images/stamp/stamp038.png",
-                        "http://pic.twitter.com/E9kjtoqJRR"
-                }, {
-                        "/images/stamp/stamp039.png",
-                        "http://pic.twitter.com/CQkpQ8Altc"
-                }, {
-                        "/images/stamp/stamp040.png",
-                        "http://pic.twitter.com/sA362F6rix"
-                }, {
-                        "/images/stamp/stamp041.png",
-                        "http://pic.twitter.com/Gg4ehjqB97"
-                }, {
-                        "/images/stamp/stamp042.png",
-                        "http://pic.twitter.com/i0LrCSSpzB"
-                }, {
-                        "/images/stamp/stamp043.png",
-                        "http://pic.twitter.com/YaskkVfYyo"
-                }, {
-                        "/images/stamp/stamp044.png",
-                        "http://pic.twitter.com/Hg3WV1gKNj"
-                }, {
-                        "/images/stamp/stamp045.png",
-                        "http://pic.twitter.com/e2idANGP7m"
-                }, {
-                        "/images/stamp/stamp046.png",
-                        "http://pic.twitter.com/8ai4ePmRbt"
-                }, {
-                        "/images/stamp/stamp047.png",
-                        "http://pic.twitter.com/b8eOJc3rdn"
-                }, {
-                        "/images/stamp/stamp048.png",
-                        "http://pic.twitter.com/VHKZpeN8eU"
-                }, {
-                        "/images/stamp/stamp049.png",
-                        "http://pic.twitter.com/42hJHOGPHm"
-                }, {
-                        "/images/stamp/stamp050.png",
-                        "http://pic.twitter.com/arr2ECWUtM"
-                }, {
-                        "/images/stamp/stamp051.png",
-                        "http://pic.twitter.com/LUKtLqZeve"
-                }, {
-                        "/images/stamp/stamp052.png",
-                        "http://pic.twitter.com/v7l5tGeSdq"
-                }, {
-                        "/images/stamp/stamp053.png",
-                        "http://pic.twitter.com/uWYAxKQpmF"
-                }, {
-                        "/images/stamp/stamp054.png",
-                        "http://pic.twitter.com/a1O9gQjnx6"
-                }
-                };
+        {
+            {
+                    "/images/stamp/stamp001.png",
+                    "http://pic.twitter.com/zJf9UxVkCy"
+            }, {
+                    "/images/stamp/stamp002.png",
+                    "http://pic.twitter.com/gGWnG2T1N2"
+            }, {
+                    "/images/stamp/stamp003.png",
+                    "http://pic.twitter.com/8dnYyZqEoF"
+            }, {
+                    "/images/stamp/stamp004.png",
+                    "http://pic.twitter.com/zjx4PUhbmY"
+            }, {
+                    "/images/stamp/stamp005.png",
+                    "http://pic.twitter.com/kHnGxG95CI"
+            }, {
+                    "/images/stamp/stamp006.png",
+                    "http://pic.twitter.com/SbRtVCDOit"
+            }, {
+                    "/images/stamp/stamp007.png",
+                    "http://pic.twitter.com/zIdqC5MtxE"
+            }, {
+                    "/images/stamp/stamp008.png",
+                    "http://pic.twitter.com/yYZEcqQCGZ"
+            }, {
+                    "/images/stamp/stamp009.png",
+                    "http://pic.twitter.com/dYwZ3axpKW"
+            }, {
+                    "/images/stamp/stamp010.png",
+                    "http://pic.twitter.com/Z0yAX0QcHJ",
+            }, {
+                    "/images/stamp/stamp011.png",
+                    "http://pic.twitter.com/M6I6regjKd",
+            }, {
+                    "/images/stamp/stamp012.png",
+                    "http://pic.twitter.com/Iv8gvPUATM"
+            }, {
+                    "/images/stamp/stamp013.png",
+                    "http://pic.twitter.com/sQOagjwDRJ"
+            }, {
+                    "/images/stamp/stamp014.png",
+                    "http://pic.twitter.com/1iWz6NfP9t"
+            }, {
+                    "/images/stamp/stamp015.png",
+                    "http://pic.twitter.com/tyXVCqpQTc"
+            }, {
+                    "/images/stamp/stamp016.png",
+                    "http://pic.twitter.com/e1CILuyhgc"
+            }, {
+                    "/images/stamp/stamp017.png",
+                    "http://pic.twitter.com/jmUVSC3zXK"
+            }, {
+                    "/images/stamp/stamp018.png",
+                    "http://pic.twitter.com/2pd753IzXC"
+            }, {
+                    "/images/stamp/stamp019.png",
+                    "http://pic.twitter.com/e1B9k2MXgS"
+            }, {
+                    "/images/stamp/stamp020.png",
+                    "http://pic.twitter.com/wGwLC41tdH"
+            }, {
+                    "/images/stamp/stamp021.png",
+                    "http://pic.twitter.com/SYkLTqipqL"
+            }, {
+                    "/images/stamp/stamp022.png",
+                    "http://pic.twitter.com/wb79W80JeH"
+            }, {
+                    "/images/stamp/stamp023.png",
+                    "http://pic.twitter.com/YYwHReTUBS",
+            }, {
+                    "/images/stamp/stamp024.png",
+                    "http://pic.twitter.com/VnmJEufqY2"
+            }, {
+                    "/images/stamp/stamp025.png",
+                    "http://pic.twitter.com/6viZHnOBEn"
+            }, {
+                    "/images/stamp/stamp026.png",
+                    "http://pic.twitter.com/MMDWi2jWHB"
+            }, {
+                    "/images/stamp/stamp027.png",
+                    "http://pic.twitter.com/kDzFQfo6ty"
+            }, {
+                    "/images/stamp/stamp028.png",
+                    "http://pic.twitter.com/cuWqlvgib3"
+            }, {
+                    "/images/stamp/stamp029.png",
+                    "http://pic.twitter.com/RQdFyGXkcv"
+            }, {
+                    "/images/stamp/stamp030.png",
+                    "http://pic.twitter.com/4xZ45MTCNv"
+            }, {
+                    "/images/stamp/stamp031.png",
+                    "http://pic.twitter.com/M1zjqfk8pH"
+            }, {
+                    "/images/stamp/stamp032.png",
+                    "http://pic.twitter.com/agDkVgLd3n"
+            }, {
+                    "/images/stamp/stamp033.png",
+                    "http://pic.twitter.com/3wfkhxsCH1",
+            }, {
+                    "/images/stamp/stamp034.png",
+                    "http://pic.twitter.com/29RqnztSk9"
+            }, {
+                    "/images/stamp/stamp035.png",
+                    "http://pic.twitter.com/XiMVmrDUWp"
+            }, {
+                    "/images/stamp/stamp036.png",
+                    "http://pic.twitter.com/hXcB5g6Ygq"
+            }, {
+                    "/images/stamp/stamp037.png",
+                    "http://pic.twitter.com/XWopb3cOts"
+            }, {
+                    "/images/stamp/stamp038.png",
+                    "http://pic.twitter.com/E9kjtoqJRR"
+            }, {
+                    "/images/stamp/stamp039.png",
+                    "http://pic.twitter.com/CQkpQ8Altc"
+            }, {
+                    "/images/stamp/stamp040.png",
+                    "http://pic.twitter.com/sA362F6rix"
+            }, {
+                    "/images/stamp/stamp041.png",
+                    "http://pic.twitter.com/Gg4ehjqB97"
+            }, {
+                    "/images/stamp/stamp042.png",
+                    "http://pic.twitter.com/i0LrCSSpzB"
+            }, {
+                    "/images/stamp/stamp043.png",
+                    "http://pic.twitter.com/YaskkVfYyo"
+            }, {
+                    "/images/stamp/stamp044.png",
+                    "http://pic.twitter.com/Hg3WV1gKNj"
+            }, {
+                    "/images/stamp/stamp045.png",
+                    "http://pic.twitter.com/e2idANGP7m"
+            }, {
+                    "/images/stamp/stamp046.png",
+                    "http://pic.twitter.com/8ai4ePmRbt"
+            }, {
+                    "/images/stamp/stamp047.png",
+                    "http://pic.twitter.com/b8eOJc3rdn"
+            }, {
+                    "/images/stamp/stamp048.png",
+                    "http://pic.twitter.com/VHKZpeN8eU"
+            }, {
+                    "/images/stamp/stamp049.png",
+                    "http://pic.twitter.com/42hJHOGPHm"
+            }, {
+                    "/images/stamp/stamp050.png",
+                    "http://pic.twitter.com/arr2ECWUtM"
+            }, {
+                    "/images/stamp/stamp051.png",
+                    "http://pic.twitter.com/LUKtLqZeve"
+            }, {
+                    "/images/stamp/stamp052.png",
+                    "http://pic.twitter.com/v7l5tGeSdq"
+            }, {
+                    "/images/stamp/stamp053.png",
+                    "http://pic.twitter.com/uWYAxKQpmF"
+            }, {
+                    "/images/stamp/stamp054.png",
+                    "http://pic.twitter.com/a1O9gQjnx6"
+            }
+        };
 
         for (String[] str : stringList) {
             ListItem item = new ListItem();
@@ -234,7 +234,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/shule517/mouneyou/TweetTask.java
@@ -0,0 +1,106 @@
+package shule517.mouneyou;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Created by shule517 on 2015/09/27.
+ */
+public
+// Image取得用スレッドクラス
+class TweetTask extends AsyncTask<String, Void, Boolean> {
+
+    String text;
+
+    TweetTask(String text) {
+        this.text = text;
+    }
+
+    private void tweet() {
+        try {
+
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("wgrDtnR6zi3sv2d6m6k3C9olS")
+                    .setOAuthConsumerSecret("mZGlg1tOgElsoX4Ge7ymZzGlMaj2IRG8l3pWsxGQYVU0ECYKAZ")
+                    .setOAuthAccessToken("67637197-6Em5LcQ197vANf1UQ1PpDvoKpIF83CImN2lozKDDz")
+                    .setOAuthAccessTokenSecret("IT27ijIQNk7Rzz2kWfvmfxTQBSZ5QtidnqDBBxhkxQqy4");
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            // Twitter twitter = new TwitterFactory().getInstance();
+
+            try {
+                // get request token.
+                // this will throw IllegalStateException if access token is already available
+                RequestToken requestToken = twitter.getOAuthRequestToken();
+                System.out.println("Got request token.");
+                System.out.println("Request token: " + requestToken.getToken());
+                System.out.println("Request token secret: " + requestToken.getTokenSecret());
+                AccessToken accessToken = null;
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                while (null == accessToken) {
+                    System.out.println("Open the following URL and grant access to your account:");
+                    System.out.println(requestToken.getAuthorizationURL());
+                    System.out.print("Enter the PIN(if available) and hit enter after you granted access.[PIN]:");
+                    String pin = br.readLine();
+                    try {
+                        if (pin.length() > 0) {
+                            accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                        } else {
+                            accessToken = twitter.getOAuthAccessToken(requestToken);
+                        }
+                    } catch (TwitterException te) {
+                        if (401 == te.getStatusCode()) {
+                            System.out.println("Unable to get the access token.");
+                        } else {
+                            te.printStackTrace();
+                        }
+                    }
+                }
+                System.out.println("Got access token.");
+                System.out.println("Access token: " + accessToken.getToken());
+                System.out.println("Access token secret: " + accessToken.getTokenSecret());
+            } catch (IllegalStateException ie) {
+                // access token is already available, or consumer key/secret is not set.
+                if (!twitter.getAuthorization().isEnabled()) {
+                    System.out.println("OAuth consumer key/secret is not set.");
+                    System.exit(-1);
+                }
+            }
+            twitter4j.Status status = twitter.updateStatus(text);
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+            System.exit(-1);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("Failed to read the system input.");
+            System.exit(-1);
+        }
+    }
+
+    @Override
+    protected Boolean doInBackground(String... params) {
+        tweet();
+        return true;
+    }
+/*
+    @Override
+    protected void onPostExecute(Bitmap result) {
+    }
+*/
+}
